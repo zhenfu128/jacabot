fe9dbe6e5ce846f30c803cb4b8a70bb81cbf5006
Karol Orlamowski <karol.orlamowski@gmail.com>
2021-04-13 21:52:32 +0200

    added control center and jacabot_server files

--- jacabot_server/src/contact/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from .views import contactView, successView
-
-urlpatterns = [
-    path('contact/', contactView, name='contact'),
-    path('success/', successView, name='success'),
-]
--- jacabot_server/src/pages/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='homepage'),
-    path('about/', views.about, name='about'),
-]
